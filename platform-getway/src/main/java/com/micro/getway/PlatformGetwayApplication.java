0d4ad2a7ea061cf46556b241b3c834835272daac
wuwenjun <784404847@qq.com>
2020-08-27 13:19:06 +0800

    feture 基础环境搭建

--- platform-getway/src/main/java/com/micro/getway/PlatformGetwayApplication.java
+++ platform-getway/src/main/java/com/micro/getway/PlatformGetwayApplication.java
@@ -1,4 +1,4 @@
-package getway;
+package com.micro.getway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- platform-getway/src/test/java/com/micro/getway/PlatformGetwayApplicationTests.java
+++ platform-getway/src/test/java/com/micro/getway/PlatformGetwayApplicationTests.java
@@ -1,4 +1,4 @@
-package getway;
+package com.micro.getway;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
