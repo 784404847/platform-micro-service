8bfc8ca7c0afcfb2dd587d6d95a92d465fedd87e
wuwenjun <784404847@qq.com>
2020-08-29 09:51:25 +0800

    feture 基础环境搭建

---
+++ platform-transaction/src/main/java/com/mservice/transaction/PlatformTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.mservice.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformTransactionApplication.class, args);
+    }
+
+}
---
+++ platform-transaction/src/test/java/com/mservice/transaction/PlatformTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mservice.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
